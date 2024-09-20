9340ceebd835fa0d00c9413b690421a3f398618e
Jpedrobarbosa <jpmbarbosa48@gmail.com>
2024-09-20 10:28:57 -0300

    Fogao

--- Fogao/Fogao.java
+++ Fogao/Fogao.java
@@ -5,7 +5,6 @@ public class Fogao {
     private int quantidadeBocas;
     private String tipoCombustivel;
 
-    // Construtor
     public Fogao(String marca, String modelo, int quantidadeBocas, String tipoCombustivel) {
         this.marca = marca;
         this.modelo = modelo;
@@ -13,7 +12,6 @@ public class Fogao {
         this.tipoCombustivel = tipoCombustivel;
     }
 
-    // Métodos de acesso (getters)
     public String getMarca() {
         return marca;
     }
@@ -31,14 +29,13 @@ public class Fogao {
     }
 
     public String fogaoLigado() {
-        return "O fogão foi ligado.";
+        return "O fogao foi ligado.";
     }
 
     public String fogaoDesligado() {
-        return "O fogão foi desligado.";
+        return "O fogao foi desligado.";
     }
 
-    // Métodos para alterar as propriedades (setters)
     public void setMarca(String marca) {
         this.marca = marca;
     }
@@ -55,35 +52,28 @@ public class Fogao {
         this.tipoCombustivel = tipoCombustivel;
     }
 
-    // Método principal (main) para testar a classe Fogao
     public static void main(String[] args) {
-        // Criando um objeto da classe Fogao
         Fogao fogao1 = new Fogao("Brastemp", "Clean", 4, "Gás");
 
-        // Exibindo as características do fogão usando os getters
-        System.out.println("Marca do fogão: " + fogao1.getMarca());
-        System.out.println("Modelo do fogão: " + fogao1.getModelo());
+        System.out.println("Marca do fogao: " + fogao1.getMarca());
+        System.out.println("Modelo do fogao: " + fogao1.getModelo());
         System.out.println("Quantidade de bocas: " + fogao1.getQuantidadeBocas());
         System.out.println("Tipo de combustível: " + fogao1.getTipoCombustivel());
 
-        // Ligando e desligando o fogão
         System.out.println(fogao1.fogaoLigado());
         System.out.println(fogao1.fogaoDesligado());
 
-        // Alterando algumas propriedades usando os setters
         fogao1.setMarca("Electrolux");
         fogao1.setModelo("Premium");
         fogao1.setQuantidadeBocas(6);
-        fogao1.setTipoCombustivel("Elétrico");
+        fogao1.setTipoCombustivel("Eletrico");
 
-        // Exibindo as novas características do fogão após alterações
-        System.out.println("\nApós as alterações:");
-        System.out.println("Marca do fogão: " + fogao1.getMarca());
-        System.out.println("Modelo do fogão: " + fogao1.getModelo());
+        System.out.println("\nApos as alteracoes:");
+        System.out.println("Marca do fogao: " + fogao1.getMarca());
+        System.out.println("Modelo do fogao: " + fogao1.getModelo());
         System.out.println("Quantidade de bocas: " + fogao1.getQuantidadeBocas());
-        System.out.println("Tipo de combustível: " + fogao1.getTipoCombustivel());
+        System.out.println("Tipo de combustivel: " + fogao1.getTipoCombustivel());
 
-        // Ligando e desligando o fogão novamente
         System.out.println(fogao1.fogaoLigado());
         System.out.println(fogao1.fogaoDesligado());
 }
